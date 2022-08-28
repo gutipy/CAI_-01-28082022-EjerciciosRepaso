22414ad759fa6baa113dc55b890e7a2c5e479017
Agustin Gutierrez <58868512+gutipy@users.noreply.github.com>
2022-08-28 16:47:33 -0300

    Repaso - Ejercicio I
    1er set de cambios correspondientes a ejercicios repaso

---
+++ EjerciciosRepaso/EjerciciosRepaso.InterfazConsola/EjercicioI.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosRepaso.InterfazConsola
+{
+    public class EjercicioI
+    {
+        public void RepasoEj1()
+        {
+            //Declaración de variables
+            int _contador;
+            int _nroRegistro;
+            string acumulador = "";
+
+
+            //Pido al usuario que ingrese su número de registro
+            Console.WriteLine("Ingrese los últimos tres dígitos de su número de registro");
+            _nroRegistro = Convert.ToInt32(Console.ReadLine());
+
+            //Si los 3 digitos ingresados comienzan con cero (000 a 099) se le suma 100 al nro de registro
+            if(_nroRegistro >= 0 && _nroRegistro <= 99)
+            {
+                _nroRegistro = _nroRegistro + 100;
+            }
+
+            //Estructura iterativa para ir contando los números y mostrarlos en pantalla
+            for(_contador = 1; _contador <= _nroRegistro; _contador++)
+            {
+                if(_contador % 3 == 0 && _contador % 5 == 0)
+                {
+                    acumulador += _contador + " - FooBar" + Environment.NewLine;
+                }
+
+                else if(_contador % 3 == 0)
+                {
+                    acumulador += _contador + " - Foo" + Environment.NewLine;
+                }
+
+                else if (_contador % 5 == 0)
+                {
+                    acumulador += _contador + " - Bar" + Environment.NewLine;
+                }
+
+                else
+                {
+                    acumulador += _contador + " - " + Environment.NewLine;
+                }
+            }
+
+            //Muestro los datos en pantalla
+            Console.WriteLine(acumulador);
+        }
+    }
+}
---
+++ EjerciciosRepaso/EjerciciosRepaso.InterfazConsola/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EjerciciosRepaso.InterfazConsola
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            EjercicioI E1 = new EjercicioI();
+
+            E1.RepasoEj1();
+
+            Console.ReadKey();
+        }
+    }
+}
