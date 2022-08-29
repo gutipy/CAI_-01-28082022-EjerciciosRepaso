6ab3b88767731928dab6f1204e8208d6b1c8ffe2
Agustin Gutierrez <58868512+gutipy@users.noreply.github.com>
2022-08-29 17:35:48 -0300

    Repaso - Ejercicio VIII
    8vo set de cambios correspondientes a ejercicios repaso

--- EjerciciosRepaso/EjerciciosRepaso.InterfazConsola/EjercicioVIII.cs
+++ EjerciciosRepaso/EjerciciosRepaso.InterfazConsola/EjercicioVIII.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -10,7 +11,20 @@ namespace EjerciciosRepaso.InterfazConsola
     {
         public void RepasoEj8()
         {
+            //Declaración de variables
+            DateTime _fecha;
+            DateTime _hoy = DateTime.Now;
+            double _difFecha;
 
+            //Pido al usuario que ingrese una fecha
+            Console.WriteLine("Ingrese una fecha");
+            _fecha = Convert.ToDateTime(Console.ReadLine());
+
+            //Calculo la diferencia entre fecha ingresada y la fecha de hoy
+            _difFecha = ((_hoy - _fecha).Days);
+
+            //Muestro en pantalla la diferencia en días
+            Console.WriteLine("La diferencia entre la fecha ingresada y el día de hoy es de " + _difFecha + " días");
         }
     }
 }
